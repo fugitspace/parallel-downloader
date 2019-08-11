ea222994768146369c08594215ec698b075cb04c
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-08-11 15:52:35 -0400

    add logic for last, first and play. need to fix play logic

--- gui/qt/mainapp.py
+++ gui/qt/mainapp.py
@@ -5,10 +5,10 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QAction, qApp, QMenu, QTextE
     QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QDesktopWidget)
 from PyQt5.QtGui import QIcon, QPixmap, QImage
 from PyQt5.QtCore import QFileInfo, Qt
-
+import time
 
 root = QFileInfo(__file__).absolutePath()
-PICTURES_DIR='/Users/public/Pictures' # change this to some directory
+PICTURES_DIR='/some/dir/name/here' # change this to some directory
 
 class App(QWidget):
 
@@ -16,57 +16,75 @@ class App(QWidget):
         super().__init__()
         self.images = [entry.path for entry in os.scandir(PICTURES_DIR)]
         self.current_image = -1
-        
+        self.is_playing = False
+        self.is_paused = False
+        self.stop = False
         self.initUI()
 
+
     @property
-    def next_image(self):
+    def next_image_location(self):
         if self.current_image == len(self.images) - 1:
             self.current_image = -1
         self.current_image += 1
         return self.images[self.current_image]
 
+
     @property
-    def prev_image(self):
+    def prev_image_location(self):
         if self.current_image == 0:
             self.current_image = len(self.images)
         self.current_image -= 1
         return self.images[self.current_image]
 
+        
+    @property
+    def last_img_location(self):
+        return self.images[len(self.images)-1]
+
+
+    @property
+    def first_img_location(self):
+        return self.images[0]
+
+
     def initUI(self):
         grid = QGridLayout()
-
         # Controls
         controls_box = QVBoxLayout()
-        controls_box.addStretch(1)
+        #controls_box.addStretch(1)
         next_btn = QPushButton('Next', self)
         next_btn.clicked.connect(self.next_img)
         prev_btn = QPushButton('Prev', self)
         prev_btn.clicked.connect(self.prev_img)
-        play_btn = QPushButton('Play', self)
         last_btn = QPushButton('Last', self)
+        last_btn.clicked.connect(self.last_img)
         first_btn =QPushButton('First', self)
+        first_btn.clicked.connect(self.first_img)
+        self.play_or_pause_btn = QPushButton('Play', self)
+        self.play_or_pause_btn.clicked.connect(self.play_or_pause)
 
         controls_box.addWidget(next_btn)
         controls_box.addWidget(prev_btn)
-        controls_box.addWidget(play_btn)
+        controls_box.addWidget(self.play_or_pause_btn)
         controls_box.addWidget(last_btn)
         controls_box.addWidget(first_btn)
 
-        # image
-        self.img = QLabel(self)
-        self.img.setPixmap(QPixmap(self.next_image))
-        # albums list
-        albums = QHBoxLayout()
-        albums.addStretch(1)
+        # image display
+        self.img = QLabel(self) # for Q, we can display on image on a Label
+        self.img.setScaledContents(True) # to maintain aspect ratio
+        self.img.setPixmap(QPixmap(self.next_image_location)) # setting the image to display as Pixmap
+        # albums list 
+        self.albums = QHBoxLayout()
+        #albums.addStretch(1)
 
         for i in range(1, 5):
-            albums.addWidget(QPushButton(f"Albamu {i}"))
+            self.albums.addWidget(QPushButton(f"Albamu {i}"))
 
         #packing
         grid.addWidget(self.img, 1, 0, 5, 1, alignment=Qt.AlignCenter)
         grid.addLayout(controls_box, 1, 1, 5, 1, alignment=Qt.AlignCenter)
-        grid.addLayout(albums, 6, 0, 1, 1, alignment=Qt.AlignCenter)
+        grid.addLayout(self.albums, 6, 0, 1, 2, alignment=Qt.AlignCenter)
         self.setLayout(grid)
         self.setWindowIcon(QIcon(f'{root}/imgs/logo.png'))
         self.setWindowTitle('Albamu ya Picha')
@@ -74,10 +92,52 @@ class App(QWidget):
         self.show()
 
     def next_img(self):
-        self.img.setPixmap(QPixmap(self.next_image))
+        """Set the next image as Pixmap to display"""
+        self.img.setPixmap(QPixmap(self.next_image_location))
 
     def prev_img(self):
-        self.img.setPixmap(QPixmap(self.prev_image))
+        """Set the previous image as Pixmap to display"""
+        self.img.setPixmap(QPixmap(self.prev_image_location))
+    
+    def last_img(self):
+        """Set the last image as Pixmap to display"""
+        self.img.setPixmap(QPixmap(self.last_img_location))
+
+    def first_img(self):
+        """Set the first image as Pixmap to display"""
+        self.img.setPixmap(QPixmap(self.first_img_location))
+
+    def play_or_pause(self):
+        # the program has just been started
+        if not self.is_playing and not self.is_paused:
+            self.play_or_pause_btn.setText("Pause")
+            # start playing
+            self.is_playing = True
+            self.is_paused = False
+        
+        # button is clicked when the player is paused, we want the next option 
+        # #to be play and we resume playing, and get out of paused status
+        elif self.is_playing:
+            self.play_or_pause_btn.setText("Play")
+            # stop playing
+            self.is_playing = False
+            # next command will be Play
+            self.is_paused = True
+        # button is clicked, and it is playing. we want to pause playing
+        elif not self.is_playing:
+            self.play_or_pause_btn.setText("Pause")
+            # start playing
+            self.is_playing = True
+            # next command is to pause
+            self.is_paused = False
+        #self.play()
+
+    def play(self):
+        while self.is_playing:
+            for img in self.images:
+                self.img.setPixmap(QPixmap(img))
+        else:
+            print("No more images")
 
     def center(self):
         """
